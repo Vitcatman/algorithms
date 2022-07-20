f4def2058425e79c453e25f7bbea4c70eb1ed5c8
Vitalii Kotov <naderevniu@ukr.net>
2022-07-20 14:34:02 +0300

    string reverse + fibonacci

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,10 +1,56 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { useState } from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { Input } from '../ui/input/input';
+import { Button } from '../ui/button/button';
+import styles from './fibonacci-page.module.css';
+import { timeout } from '../../utils';
+import { SHORT_DELAY_IN_MS } from '../../constants/delays';
+import { Circle } from '../ui/circle/circle'
 
 export const FibonacciPage: React.FC = () => {
+
+  const [input, setInput] = useState<number>(0);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [calculatedArray, setCalculatedArray] = useState<number[]>([]);
+
+  const handleInput = (evt: React.SyntheticEvent<HTMLInputElement>) => {
+    setInput(parseInt(evt.currentTarget.value))
+  }
+
+  const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    await getFibonacciSequence(input)
+  }
+
+  const getFibonacciSequence = async (number: number) => {
+    setIsLoading(true);
+  let arr: number[] = [];
+  for (let i = 0; i <= number; i++) {
+    if (i < 2) {
+      arr.push(1);
+      await timeout(SHORT_DELAY_IN_MS);
+      setCalculatedArray([...arr])
+    } else {
+      arr.push(arr[i - 2] + arr[i - 1]);
+      await timeout(SHORT_DELAY_IN_MS);
+      setCalculatedArray([...arr])
+    } 
+  }
+  setIsLoading(false);
+  }
+
   return (
-    <SolutionLayout title="Последовательность Фибоначчи">
-     
+    <SolutionLayout title='Последовательность Фибоначчи'>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input max={19} type='number' isLimitText={true} onChange={(evt) => handleInput(evt)} />
+        <Button isLoader={isLoading} text='Рассчитать' type='submit' />
+      </form>
+      <div className={styles.circle}>
+           {calculatedArray.map((item, i) => {
+               console.log(item)
+               return <Circle key={i} letter={item+''} index={i}/>
+            })}
+      </div>
     </SolutionLayout>
   );
 };
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,76 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { useState } from 'react';
+import { SolutionLayout } from '../ui/solution-layout/solution-layout';
+import { Input } from '../ui/input/input';
+import { Button } from '../ui/button/button';
+import styles from './string.module.css';
+import { timeout } from '../../utils';
+import { DELAY_IN_MS } from '../../constants/delays';
+import { ElementStates } from '../../types/element-states';
+import { Circle } from '../ui/circle/circle'
+
+interface ILetter {
+ name: string,
+ status: ElementStates
+}
 
 export const StringComponent: React.FC = () => {
+  const [input, setInput] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [reversedArray, setReversedArray] = useState< ILetter[]>([]);
+   
+  const handleInput = (evt: React.SyntheticEvent<HTMLInputElement>) => {
+    setInput(evt.currentTarget.value)
+  }
+
+  const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault()
+    const arrayToReverse = input.split('').map((el) => {return {
+        name: el,
+        status: ElementStates.Default
+    }});
+    
+    await reverseStr(arrayToReverse)
+  }
+
+  const reverseStr = async (array: ILetter[]) => {
+    setIsLoading(true);
+    let start = 0;
+    let end = array.length - 1;
+
+    while (start <= end) {
+        const tempStart = array[start];
+        const tempEnd = array [end];
+        array[start].status = ElementStates.Changing
+        array[end].status = ElementStates.Changing
+        setReversedArray([...array])
+
+        await timeout(DELAY_IN_MS);
+
+        array[start] = tempEnd;
+        array[end] = tempStart;
+        array[start].status = ElementStates.Modified
+        array[end].status = ElementStates.Modified
+        setReversedArray([...array])
+
+        await timeout(DELAY_IN_MS);
+
+        start++;
+        end--;
+    }
+    setIsLoading(false);
+  }
+
   return (
-    <SolutionLayout title="Строка">
-     
+    <SolutionLayout title='Строка'>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <Input maxLength={11} isLimitText={true} onChange={(evt) => handleInput(evt)} />
+        <Button isLoader={isLoading} text='Развернуть' type='submit' />
+      </form>
+      <div className={styles.circle}>
+            {reversedArray.map((item, i) => {
+               return <Circle key={i} letter={item.name} state={item.status}/>
+            })}
+        </div>
     </SolutionLayout>
   );
 };
