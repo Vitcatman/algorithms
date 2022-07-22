98a81e5627252b4616fa3d1e856691adf18ebe35
Vitalii Kotov <naderevniu@ukr.net>
2022-07-22 17:24:33 +0300

    bubble sorting finished

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,9 @@
 export const timeout = (ms: number) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export function swap <T> (arr: Array<T>, firstIndex: number, secondIndex: number) {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+};
